ffe89a182c70a2859d74b0daa009589e51b09abe
Christopher Homberger <christopher.homberger@web.de>
2025-10-23 01:06:27 +0200

    ...

--- runtime/task.go
+++ runtime/task.go
@@ -416,6 +416,7 @@ func (t *Task) Run(ctx context.Context, task *runnerv1.Task, runnerWorker []stri
 					}
 				} else if strings.Contains(err.Error(), "Unauthenticated") {
 					log.Errorf("failed to update log: %v, has been removed", err)
+					rows = []*runnerv1.LogRow{}
 				} else {
 					log.Errorf("failed to update log: %v, batching later", err)
 				}
